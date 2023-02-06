c9cdb090a03e95f10ad638325fb137e93e2ee645
Kayle Abesamis <jinxlearns2code@gmail.com>
2023-02-06 23:09:50 +1100

    added image carousel

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active')
 })
+
+let counter = 1;
+setInterval(() => {
+	document.getElementById('radio' + counter).checked = true;
+	counter++;
+	if (counter > 4) {
+		counter = 1;
+	}
+	}, 10000)
